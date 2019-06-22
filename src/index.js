004ea8450c2873fbf8f9f3f9acb0b52b423ef6cf
josue kula <koolmx9@gmail.com>
2019-06-22 18:23:00 -0500

    interceptors with axios

--- src/index.js
+++ src/index.js
@@ -7,8 +7,22 @@ import axios from 'axios';
 
 axios.interceptors.request.use(request => {
     console.log(request);
+    // edit the request config
     return request;
+}, err => {
+    console.error(err);
+    return Promise.reject(err);
 })
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // edit the request config
+    return response;
+}, err => {
+    console.log(err);
+    return Promise.reject(err);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
