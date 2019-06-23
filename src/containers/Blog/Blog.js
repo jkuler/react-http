2fa9110cc5d923b38bc0007c86b18f64211aab8a
josue kula <koolmx9@gmail.com>
2019-06-23 13:57:43 -0500

    switching pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import  { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 // import axios from 'axios';
 import axios from '../../axios'
@@ -33,8 +34,10 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home 1</h1>} />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 1</h1>} /> */}
+        <Route path="/" exact component={ Posts } />
+        <Route path="/new-post" exact component={ NewPost } />
       </div>
     );
   }
