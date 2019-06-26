a8f9ad895714e54a341c4a85f1dc2cfee08b4fe8
josue kula <koolmx9@gmail.com>
2019-06-25 21:14:09 -0500

    handling 404

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,7 @@ class Blog extends Component {
 state = {
       auth: false
     }
-    
+
   // async componentDidMount() {
   //   const { data: response } = await axios.get(
   //     'https://jsonplaceholder.typicode.com/posts',
@@ -55,7 +55,8 @@ state = {
         <Switch> 
           { this.state.auth ? <Route path="/new-post"  component={ NewPost } /> : null } 
           <Route path="/posts" component={ Posts } /> 
-          <Redirect from="/" to="/posts" /> 
+          <Route render={() => <h1>Not Found</h1>} />
+          {/* <Redirect from="/" to="/posts" />  */}
           {/* <Route path="/" component={ Posts } />   */}
         </Switch>
         
